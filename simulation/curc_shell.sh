da845d12cd4e7f14a14e82a0151e555c9f64ded6
Yu-Wen Chen <r05223131@ntu.edu.tw>
2023-12-03 14:14:38 -0700

    nadir zpt test

---
+++ simulation/curc_shell.sh
@@ -0,0 +1,17 @@
+#!/bin/env python
+
+#SBATCH --partition=amilan
+#SBATCH --nodes=1
+#SBATCH --ntasks=32
+#SBATCH --time=24:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=Yu-Wen.Chen@colorado.edu
+#SBATCH --output=sbatch-output_%x_%j.txt
+#SBATCH --job-name=OCO2_test
+#SBATCH --account=ucb427_asc1
+
+
+module_init
+conda activate er3t
+
+python simulation.py
